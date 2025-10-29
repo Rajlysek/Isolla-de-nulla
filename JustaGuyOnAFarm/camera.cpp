71941af5c81f971812a0c9f1db41d9c638a07669
Rajlysek <vanessa.peslova@seznam.cz>
2025-10-29 23:51:54 +0100

    comments

--- JustaGuyOnAFarm/camera.cpp
+++ JustaGuyOnAFarm/camera.cpp
@@ -19,9 +19,9 @@ void camera::borderCollisionView(float playerCenterX, float playerCenterY, float
 	int biggerXHalf = backgroundWidth - lowerXHalf;
 	int biggerYHalf = backgroundHeight - lowerYHalf;
 	sf::Vector2f center = view.getCenter();
-	center.x = playerCenterX;
+	center.x = playerCenterX; //set camera center to player center, if not in border collision area
 	center.y = playerCenterY;
-	if(playerCenterX <= lowerXHalf) center.x = lowerXHalf;
+	if (playerCenterX <= lowerXHalf) center.x = lowerXHalf; // if player center position is less than half screen size, set camera center to half screen size
 	if (playerCenterY <= lowerYHalf) center.y = lowerYHalf;
 
 	if (playerCenterX >= biggerXHalf) center.x = biggerXHalf;
